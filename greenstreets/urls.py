76791c361705c9ef3da7087900ef15e1675683ef
Owen Lynch <oclynch888@gmail.com>
2013-06-26 17:45:45 -0400

    updated urls

--- greenstreets/urls.py
+++ greenstreets/urls.py
@@ -3,8 +3,8 @@ from django.views.generic import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
 
@@ -18,9 +18,14 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+   # url(r'^admin/', include(admin.site.urls)),
 
     (r'^grappelli/', include('grappelli.urls')),
+    url(r'^leaderboard/(?P<month>[-\w]+)/(?P<vol_v_perc>\w+)/(?P<size>\w+)/(?P<sector>\w+)/(?P<company>\w+)/$', 'leaderboard.views.leaderboard', name='leaderboard'),
+    url(r'^leaderboard/(?P<month>[-\w]+)/(?P<vol_v_perc>\w+)/(?P<size>\w+)/(?P<sector>\w+)/$', 'leaderboard.views.leaderboard'),
+    url(r'^leaderboard/$', 'leaderboard.views.leaderboard'),
+    url(r'^leaderboard-bare', 'leaderboard.views.leaderboard_bare'),
+    url(r'^test/(?P<text>\w+)/(?P<test>\w+)/$', 'leaderboard.views.test')
 )
 
 urlpatterns += staticfiles_urlpatterns()
