2756987d4eba6901f176faa84f16ea1b26ecc237
Owen Lynch <oclynch888@gmail.com>
2013-07-17 19:51:58 -0400

    added /emplbreakdown

---
+++ leaderboardlist/views.py
@@ -0,0 +1,69 @@
+# Create your views here.
+from survey.models import Employer, Commutersurvey
+from leaderboard.views import getBreakDown, getCanvasJSChart
+from django.shortcuts import render
+from leaderboard.models import Month
+import json
+
+def getCJSEmplList(month):
+    chart = {
+        'title': { 'text': 'Alphabetical Employer Breakdown for %s' % (month,), 'fontSize': 20 },
+        'creditText': 'gogreenstreets.org',
+        'creditHref': 'http://gogreenstreets.org',
+        'axisX': {
+            'labelFontSize': 12,
+            'margin': 0,
+        },
+        'axisY': {
+            'labelFontSize': 12,
+        },
+        'legend': {
+            'fontSize': 12,
+        },
+        'data': [
+            {
+                'type': 'stackedBar',
+                'color': '#0096FF',
+                'legendText': 'Green Switches',
+                'showInLegend': 'true',
+                'dataPoints': [
+                ]
+            },
+            {
+                'type': 'stackedBar',
+                'color': '#65AB4B',
+                'legendText': 'Green Commuters',
+                'showInLegend': 'true',
+                'dataPoints': [
+                ]
+            },
+            {
+                'type': 'stackedBar',
+                'color': '#FF2600',
+                'legendText': 'Car Commuters',
+                'showInLegend': 'true',
+                'dataPoints': [
+                ]
+            },
+            {
+                'type': 'stackedBar',
+                'color': '#9437FF',
+                'legendText': 'Unhealthy Switches',
+                'showInLegend': 'true',
+                'dataPoints': [
+                ]
+            },
+        ]
+    }
+    breakDownTranslator = [ 'gs', 'gc', 'cc', 'us' ]
+    for emp in Employer.objects.filter(active=True).reverse():
+        breakDown = getBreakDown(emp, month)
+        for i in range(0, 4):
+            chart['data'][i]['dataPoints'] += [{ 'label': emp.name, 'y': breakDown[breakDownTranslator[i]] },]
+    return json.dumps(chart)
+
+def empBreakDown(request, month):
+    fullMonth = Month.objects.get(url_month=month)
+    context = { 'month': month, 'CHART_DATA': getCJSEmplList(fullMonth.month) }
+    return render(request, 'leaderboardlist/leaderboardlist.html', context)
+    
