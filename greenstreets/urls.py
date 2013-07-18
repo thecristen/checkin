8cd012980ef4b7616806ff31f3859321d8529f44
Owen Lynch <oclynch888@gmail.com>
2013-07-18 09:42:08 -0400

    added mbp urls.py

--- greenstreets/urls.py
+++ greenstreets/urls.py
@@ -3,8 +3,8 @@ from django.views.generic import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 
@@ -18,13 +18,16 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-   # url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 
     (r'^grappelli/', include('grappelli.urls')),
-    url(r'^leaderboard/(?P<month>[-\w]+)/(?P<vol_v_perc>\w+)/(?P<size>\w+)/(?P<sector>\w+)/(?P<company>\w+)/$', 'leaderboard.views.leaderboard', name='leaderboard'),
-    url(r'^leaderboard/(?P<month>[-\w]+)/(?P<vol_v_perc>\w+)/(?P<size>\w+)/(?P<sector>\w+)/$', 'leaderboard.views.leaderboard'),
+    url(r'^leaderboard/(?P<month>[-\w]+)/(?P<vol_v_perc>\w+)/(?P<svs>\w+)/(?P<sos>\w+)/(?P<company>\w+)/$', 'leaderboard.views.leaderboard', name='leaderboard'),
+    url(r'^leaderboard/(?P<month>[-\w]+)/(?P<vol_v_perc>\w+)/(?P<svs>\w+)/(?P<sos>\w+)/$', 'leaderboard.views.leaderboard'),
+    url(r'^leaderboard-bare/(?P<month>[-\w]+)/(?P<vol_v_perc>\w+)/(?P<svs>\w+)/(?P<sos>\w+)/$', 'leaderboard.views.leaderboard_bare'),
     url(r'^leaderboard/$', 'leaderboard.views.leaderboard'),
-    url(r'^leaderboard-bare', 'leaderboard.views.leaderboard_bare'),
+    url(r'^leaderboard-bare/$', 'leaderboard.views.leaderboard_bare'),
+    url(r'^test-chart/$', 'leaderboard.views.testchart'),
+    url(r'^emplbreakdown/(?P<month>[-\w]+)/$', 'leaderboardlist.views.empBreakDown'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
