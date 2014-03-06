ba1a5521f6b23889a10c8e6037103f9f6c235fdf
Christian Spanring <cspanring@gmail.com>
2014-03-05 21:11:20 -0500

    cleanup: removes student model classes

--- survey/views.py
+++ survey/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 from django.utils import simplejson
 from django.forms.models import inlineformset_factory
 
-from survey.models import School, Schooldistrict, Commutersurvey, Employer, Studentsurvey, Studentgroup
-from survey.forms import CommuterForm, StudentForm, StudentgroupForm
+from survey.models import Commutersurvey, Employer
+from survey.forms import CommuterForm
 
 
 def process_request(request):
@@ -16,25 +16,7 @@ def process_request(request):
     if 'HTTP_X_REAL_IP' in request.META:
         request.META['REMOTE_ADDR'] = request.META['HTTP_X_REAL_IP']
     return request
-    
-
-def get_schools(request, slug):
-    """
-    Returns all schools for given district as JSON
-    """
-    
-    # check if exists
-    district = get_object_or_404(Schooldistrict.objects, slug=slug)
-    
-    schools = School.objects.filter(districtid=district).order_by('name')
-    
-    # build slim response data
-    response = []
-    for school in schools:
-        response.append(dict(name=school.name, id=school.id))
-
-    return HttpResponse(simplejson.dumps(response), mimetype='application/json')   
-  
+      
 
 def commuter(request):
     """
@@ -75,51 +57,5 @@ def commuter(request):
         surveyform = CommuterForm(instance=survey)
         return render_to_response('survey/commuterform.html', locals(), context_instance=RequestContext(request))
 
-
-def student(request):
-    """ 
-    Renders Teacherform or saves it if POST
-    """
-
-    request = process_request(request)
-
-    # get all school districts with participating schools
-    districts = Schooldistrict.objects.filter(school__survey_active=True).distinct()
-
-    survey = Studentsurvey()
-
-    SurveyFormset = inlineformset_factory(Studentsurvey, Studentgroup, form=StudentgroupForm, extra=1, can_delete=False)
-
-    if request.method == 'POST':
-        surveyform = StudentForm(request.POST, instance=survey)
-        surveyformset = SurveyFormset(request.POST, instance=survey)
-        survey.ip = request.META['REMOTE_ADDR']
-
-        # check if user already checked in this month
-        month = request.POST['month']
-        teacher_email = request.POST['teacher_email']
-        
-        # duplicate check, disabled per Nicole's request
-        # if Studentsurvey.objects.filter(month__iexact=month, teacher_email__iexact=teacher_email).exists():
-        #     existing_survey = Studentsurvey.objects.filter(month__iexact=month, teacher_email__iexact=teacher_email).order_by('-created')[0]
-        #     # remove all related objects
-        #     existing_studentgroups = existing_survey.studentgroup_set.all()
-        #     for existing_studentgroup in existing_studentgroups:
-        #         existing_studentgroup.delete()
-        #     # adding existing id forces update
-        #     survey.id = existing_survey.id
-        #     survey.created = existing_survey.created
-            
-        if surveyformset.is_valid() and surveyform.is_valid():
-            surveyform.save()
-            surveyformset.save()  
-            month = request.POST['month']
-            return render_to_response('survey/thanks.html', locals(), context_instance=RequestContext(request)) 
-        else:
-            return render_to_response('survey/studentform.html', locals(), context_instance=RequestContext(request))
-    else:
-        surveyform = StudentForm(instance=survey)
-        surveyformset = SurveyFormset(instance=survey)
-    return render_to_response('survey/studentform.html', locals(), context_instance=RequestContext(request))
 
 
